3084df68bbd07719ddc588454e8abe383a377207
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-27 04:54:10 +0000

    Add calculator functions

--- main.py
+++ main.py
@@ -3,6 +3,24 @@
 # Imports
 from art import logo
 
+
+# Basic calculator functions: Addition, Subtraction, Multiplication and Division
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
 # Default displays
 print(logo)
 
